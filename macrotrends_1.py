9438931d3948d4e2a7d598bc8ee923503451a750
JayantLodhi <jayantlodhi200@gmail.com>
2025-03-12 22:40:19 +0530

    data in table form

--- macrotrends_1.py
+++ macrotrends_1.py
@@ -20,14 +20,14 @@ all_headings = table.find_all("th") #all the heading
 
 main_headings = all_headings[0].text
 
-print(main_headings)
+#print(main_headings)
 
 headings = []   # to store all the headings as text
 
 for i in all_headings[1:]:
     headings.append(i.text)
 
-print(headings)
+#print(headings)
 
 all_values = table.find_all("td")   #all the values for the headings
 
@@ -38,4 +38,16 @@ values = []  # to store all the values as text
 for i in all_values:
     values.append(i.text)
 
-print(values)
+#print(values[0:7])
+
+print(f"\n{main_headings:_^100}\n")
+# Print header
+print(f"{headings[0]:^0} {headings[1]:^25} {headings[2]:^12} {headings[3]:^12} {headings[4]:^12} {headings[5]:^12} {headings[6]:^15}")
+
+# Print data
+x = 0
+while x < len(values):
+    row_val = values[x:x+7]
+    print(f"{row_val[0]:^0} {row_val[1]:^25} {row_val[2]:^12} {row_val[3]:^12} {row_val[4]:^12} {row_val[5]:^12} {row_val[6]:^15}")
+
+    x = x + 7
